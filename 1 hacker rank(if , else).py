02de052468f6170e961883731ab3b7437e03af9e
Ananyasen29 <90442331+Ananyasen29@users.noreply.github.com>
2021-09-26 16:17:03 +0530

    Add files via upload
    Task
    Given an integer, , perform the following conditional actions:
    
    If  is odd, print Weird
    If  is even and in the inclusive range of  2 to 5 , print Not Weird
    If  is even and in the inclusive range of 6  to 20 , print Weird
    If  is even and greater than 20, print Not Weird
    Input Format
    
    A single line containing a positive integer, .
    
    Constraints
    
    Output Format
    
    Print Weird if the number is weird. Otherwise, print Not Weird.

---
+++ 1 hacker rank(if , else).py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+n = int(input())
+if n%2==1:
+    print('Weird')
+elif n%2==0 and n in range (2,5):
+    print('Not Weird')
+elif n%2==0 and n in range (6,21):
+    print("weird")
+else :
+    if n%2==0 and n>20:
+        print('Not Weird')
+
+
+# In[ ]:
+
+
+
+
